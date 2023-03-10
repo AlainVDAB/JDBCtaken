f7b270ee62c1a8a5065fd5b5b20448c36ff06de6
Alain <alain.dossche@vdabcampus.be>
2023-03-10 12:03:15 +0100

    bieren verwijderen

---
+++ src/main/java/be/vdab/Main.java
@@ -0,0 +1,22 @@
+package be.vdab;
+
+import be.vdab.repositories.BierRepository;
+
+import java.sql.SQLException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        var repository = new BierRepository();
+
+        try {
+            System.out.print(repository.verwijderBierenMetOnbekendeAlcohol());
+            System.out.println(" bieren verwijderd.");
+        } catch (
+                SQLException ex) {
+            ex.printStackTrace(System.err);
+        }
+    }
+}
